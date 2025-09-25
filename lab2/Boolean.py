3dcd65b10757fd87eb7005cf90b0ac1070f6d7b2
Akhmet <ahmetsultanxd@gmail.com>
2025-09-25 23:17:15 +0500

    lab2

---
+++ lab2/Boolean.py
@@ -0,0 +1,27 @@
+# 1
+# 10 > 9 => True
+
+
+# 2
+# 10 == 9 => False
+
+
+# 3
+# 10 < 9 False
+
+
+# 4
+# bool("abc") => True
+
+
+# 5
+# bool(0) => False
+
+
+
+
+# Examples
+
+# print(bool())
+# print(bool(-1))
+# print(bool(None))
---
+++ lab2/Lists.py
@@ -0,0 +1,59 @@
+# 1
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits[1])
+
+
+# 2
+# fruits = ["apple", "banana", "cherry"]
+# fruits[0] = "kiwi"
+
+
+
+# 3
+# fruits = ["apple", "banana", "cherry"]
+# fruits.append("orange")
+
+
+
+# 4
+# fruits = ["apple", "banana", "cherry"]
+# fruits.insert(1,"lemon")
+
+
+
+# 5
+# fruits = ["apple", "banana", "cherry"]
+# fruits.remove("banana")
+
+
+
+# 6
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits[-1])
+
+
+
+# 7
+# fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(fruits[2:5])
+
+
+
+# 8
+# fruits = ["apple", "banana", "cherry"]
+# print(len(fruits))
+
+
+
+# Examples
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.append("orange")
+# fruits.remove("banana")
+# fruits.pop(1)
+# fruits.sort(reverse = True)
+# newlist = [x for x in fruits if "a" in x]
+# li = list(fruits)
+# li.extend(newlist)
+# fruits.clear()
+
