c4538b5ee53970ed3470026c16dab52492c4859a
riplie8 <70997863+riplie8@users.noreply.github.com>
2025-10-17 11:17:11 +0500

    Update 1.py

--- lab5/1.py
+++ lab5/1.py
@@ -2,7 +2,7 @@ from regular import replacing, matching, splitting
 
 # 1 Write a Python program that matches a string that has an 'a' followed by zero or more 'b''s.
     
-text = 'wsabb'
+text = 'aababbabaababb'
 pattern = r'.*a[b]*.*'
 
 matching(pattern, text)
